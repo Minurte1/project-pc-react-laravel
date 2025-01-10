d2d9382d983713ca4bdb08d8bbef45636beb4b19
Quốc Bảo <baoquoczero@gmail.com>
2025-01-10 18:52:07 +0700

    Update api.php

--- project-pc-backend-lavarel/routes/api.php
+++ project-pc-backend-lavarel/routes/api.php
@@ -69,8 +69,8 @@ Route::post('/save-don-hang', [AdminController::class, 'createDonHang']);
 Route::post('/update-don-hang/{id}', [AdminController::class, 'updateDonHang']);
 Route::post('/xoa-don-hang/{id}', [AdminController::class, 'deleteDonHang']);
 
-Route::post('/get-chi-tiet-don-hang/{id}', [AdminController::class, 'getChiTietHoaDon']);
-Route::post('/update-chi-tiet-don-hang/{id}', [AdminController::class, 'updateChiTietHoaDon']);
+Route::post('/get-chi-tiet-don-hang/{maCthd}', [AdminController::class, 'getChiTietHoaDon']);
+Route::post('/update-chi-tiet-don-hang/{maCthd}', [AdminController::class, 'updateChiTietHoaDon']);
 
 Route::get('/list-danh-thu', [AdminController::class, 'getListDanhThu']);
 
